5ec6864f8920e58c93378c7f4e05928640318817
gviel <g.viel@net4up.com>
2017-11-17 15:47:37 +0100

    scrapy init project

---
+++ tarentula/tarentula/spiders/youtube.py
@@ -0,0 +1,34 @@
+# scrapy runspider myspider.py -o cats.json
+
+import scrapy
+
+# Global var to count number of followed links
+nextit = 0
+
+class YoutubeSpider(scrapy.Spider):
+    name = "youtube"
+    # URL to scrap
+    start_urls = [
+        'https://www.youtube.com/results?sp=CAJQFA%253D%253D&search_query=funny+cats',
+    ]
+
+    def parse(self, response):
+        # Usong the global var
+        global nextit
+        # Get the all the div 'yt-lockup-content' 
+        contents = response.css("div.yt-lockup-content")
+        # For each, get attributes 'href' and 'title' of the 'a' element in 'h3' element
+        for content in contents:
+            url = content.css("h3 a::attr(href)").extract_first()
+            title = content.css("h3 a::attr(title)").extract_first()
+            yield { 'url' : 'https://www.youtube.com'+url, 'title' : title }
+
+        # Get the first div 'branded-page-box.search-pager.spf-link' (next links) 
+        nextbox = response.css("div.branded-page-box.search-pager.spf-link")[0]
+        # Get the last 'a' element in previous div and extract attribute 'href' of the other 'a' element within
+        nextlink = nextbox.css('a')[-1].css('a::attr(href)').extract_first()
+        # If link is present ans if we do nat have iterated enough, follow the link with parsing
+        if (nextlink is not None) and (nextit < 3) :
+            nextit += 1
+            yield response.follow(nextlink, callback=self.parse)
+
