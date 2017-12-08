750faf7a90a420f87c7b19802ea568b54e9c1477
Gabriel Viel <g.viel@net4up.com>
2017-12-08 15:45:26 +0000

    attachment

--- silk/silk.py
+++ silk/silk.py
@@ -1,10 +1,19 @@
+	
+# -*- coding: utf-8 -*- 
 import jinja2
 import smtplib
 import email.utils
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 import getpass
 import ConfigParser
+import sqlite3
+import sys
 
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 tplLoader = jinja2.FileSystemLoader(searchpath='./templates')
 tplEnv = jinja2.Environment(loader=tplLoader)
@@ -25,13 +34,32 @@ server = config.get('MAIL','server')
 user   = config.get('MAIL','username')
 password = config.get('MAIL','password')
 recipient = config.get('MAIL','recipient')
+dbpath = config.get('DB','path')
+
+conn = sqlite3.connect(dbpath)
+cursor = conn.cursor()
+cursor.execute("""
+	SELECT * FROM content
+	""")
+metadata = cursor.fetchone()
+cursor.close()
+
 
 # Create the message
-msg = MIMEText(body)
+msg = MIMEMultipart()
 msg.set_unixfrom(user)
 msg['To'] = email.utils.formataddr(('Recipient', recipient))
 msg['From'] = email.utils.formataddr(('Post', user))
-msg['Subject'] = 'Hello Cat'
+msg['Subject'] = metadata[3] + '¤' + metadata[1]
+
+msg.attach(MIMEText(body))
+
+f = 'test.jpg'
+part = MIMEBase('application', "octet-stream")
+part.set_payload( open(f,"rb").read() )
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="test.jpg"')
+msg.attach(part)
 
 server = smtplib.SMTP(server)
 try:
