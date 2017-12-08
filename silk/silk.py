b3ccc9ea17aecf34240af0eb629411c6d8eb2038
Gabriel Viel <g.viel@net4up.com>
2017-12-08 14:27:21 +0000

    mail start

--- silk/silk.py
+++ silk/silk.py
@@ -1,4 +1,10 @@
 import jinja2
+import smtplib
+import email.utils
+from email.mime.text import MIMEText
+import getpass
+import ConfigParser
+
 
 tplLoader = jinja2.FileSystemLoader(searchpath='./templates')
 tplEnv = jinja2.Environment(loader=tplLoader)
@@ -10,4 +16,35 @@ tplVars = {
 }
 
 template = tplEnv.get_template( "body.jinja" )
-print template.render(tplVars)
+body = template.render(tplVars)
+
+
+config = ConfigParser.ConfigParser()
+config.read('./mysettings.ini')
+server = config.get('MAIL','server')
+user   = config.get('MAIL','username')
+password = config.get('MAIL','password')
+
+# Create the message
+msg = MIMEText(body)
+msg.set_unixfrom(user)
+msg['To'] = email.utils.formataddr(('Recipient', 'gb.viel@gmail.com'))
+msg['From'] = email.utils.formataddr(('Post', user))
+msg['Subject'] = 'Hello Cat'
+
+server = smtplib.SMTP(server)
+try:
+    server.set_debuglevel(True)
+
+    # identify ourselves, prompting server for supported features
+    server.ehlo()
+
+    # If we can encrypt this session, do it
+    if server.has_extn('STARTTLS'):
+        server.starttls()
+        server.ehlo() # re-identify ourselves over TLS connection
+
+    server.login(user, password)
+    server.sendmail(user, 'gb.viel@gmail.com', msg.as_string())
+finally:
+    server.quit()
