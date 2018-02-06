34f8a8d3f9976108efd8edc8d295dd066b3a4272
Gabriel Viel <g.viel@net4up.com>
2018-02-06 15:36:07 +0100

    SQLite

--- tarentula/server.py
+++ tarentula/server.py
@@ -3,8 +3,45 @@
 from flask import Flask
 from flask import request
 import base64
+import sqlite3
+from contextlib import closing
+import hashlib
+
 
 app = Flask(__name__)
+
+@app.before_first_request
+def db_create():
+    try:
+
+        conn = sqlite3.connect("output.db")
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS content(
+        id TEXT PRIMARY KEY UNIQUE,
+        url TEXT,
+        title TEXT,
+        thumb TEXT,
+        posted INTEGER
+        )
+        """)
+        conn.commit()
+
+        cursor.execute("""
+        CREATE INDEX IF NOT EXISTS idx_posted
+        ON content(posted)
+        """)
+        conn.commit()
+
+    except sqlite3.Error,e:
+        raise Exception( "Error in pipeline open_spider: %s" % e)
+
+    finally:
+        cursor.close()
+        conn.close()       
+
+
 
 @app.route('/post', methods=['POST'])
 def show_post():
@@ -15,5 +52,26 @@ def show_post():
     img_data = data['img'][22:] 
 
     with open("imageToSave.png", "wb") as f:
-    	f.write(img_data.decode('base64'))
+        f.write(img_data.decode('base64'))
+
+
+    conn = sqlite3.connect("output.db")
+    cursor = conn.cursor()
+
+    try:
+        
+        cursor.execute("""
+        INSERT INTO content(id, url, title, thumb, posted) VALUES(?, ?, ?, ?, ?)""", (hashlib.sha1(data['url']).hexdigest(), data['url'], data['title'], "imageToSave.png", 0))
+        
+    except sqlite3.IntegrityError:
+        # Key already exists (id est URL already scrapped), do nothing
+        pass
+        
+    finally:
+        conn.commit()
+        cursor.close()
+        conn.close() 
+
+
     return 'OK'
+
