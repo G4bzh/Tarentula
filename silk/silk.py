5bed16e3a4080a9ddb7c8a6514fc6bedae012e60
Gabriel Viel <g.viel@net4up.com>
2018-02-16 15:22:14 +0100

    site in settings

--- silk/silk.py
+++ silk/silk.py
@@ -5,62 +5,77 @@ from mail.mail import sendpost
 from contextlib import closing
 import ConfigParser
 import sqlite3
-import sys
-
-reload(sys)  
-sys.setdefaultencoding('utf8')
-
-tplLoader = jinja2.FileSystemLoader(searchpath='./templates')
-tplEnv = jinja2.Environment(loader=tplLoader)
+import sys, getopt
 
 
-config = ConfigParser.ConfigParser()
-config.read('./mysettings.ini')
-server = config.get('MAIL','server')
-user   = config.get('MAIL','username')
-password = config.get('MAIL','password')
-recipient = config.get('MAIL','recipient')
-debug = config.get('MAIL','debug')
-dbpath = config.get('DB','path')
 
 
-try:
-    with closing(sqlite3.connect(dbpath, timeout=1)) as conn:
+def main(argv):
 
-        cursor = conn.cursor()
-        cursor.execute("""
-        	SELECT * FROM content WHERE posted=0 LIMIT 1
-        	""")
-        metadata = cursor.fetchone()
+    reload(sys)  
+    sys.setdefaultencoding('utf8')
+    section = 'CATS'
 
-        tplVars = {
-            'parts': range(1,11),
-            'lines' : range(5,8),
-            'title' : metadata[2],
-            'keywords' : ('cat', 'cats', 'kitten', 'pussy', 'kitty', 'puss')
-        }
-
-        template = tplEnv.get_template( "meta.jinja" )
-        meta = template.render(tplVars)
-
-        subject = metadata[2] + '¤' + metadata[1] + '¤' + tplVars['keywords'][0] + '¤' + meta 
-
-        template = tplEnv.get_template( "body.jinja" )
-        body = template.render(tplVars)
-
-        sendpost(recipient, user, subject, body, metadata[3], server, password, debug)
-
-        cursor.execute("""
-        	UPDATE content SET posted = 1 WHERE id = ?
-        	""",(metadata[0],))
-
-     
-        conn.commit()
-        cursor.close()
+    opts, args = getopt.getopt(argv,"s:")
+    for opt,arg in opts:
+        section = arg.upper()
 
 
-except ValueError:
-    raise Exception( "Failed to send post")
+    tplLoader = jinja2.FileSystemLoader(searchpath='./templates')
+    tplEnv = jinja2.Environment(loader=tplLoader)
 
-except sqlite3.Error,e:
-    raise Exception( "Error in silk: %s" % e)
+
+    config = ConfigParser.ConfigParser()
+    config.read('./mysettings.ini')
+    server = config.get('MAIL','server')
+    user   = config.get('MAIL','username')
+    password = config.get('MAIL','password')
+    recipient = config.get('MAIL','recipient')
+    debug = config.get('MAIL','debug')
+    dbpath = config.get(section,'dbpath')
+    kw = config.get(section,'keywords').split(",")
+
+
+    try:
+        with closing(sqlite3.connect(dbpath, timeout=1)) as conn:
+
+            cursor = conn.cursor()
+            cursor.execute("""
+            	SELECT * FROM content WHERE posted=0 LIMIT 1
+            	""")
+            metadata = cursor.fetchone()
+
+            tplVars = {
+                'parts': range(1,11),
+                'lines' : range(5,8),
+                'title' : metadata[2],
+                'keywords' : kw
+            }
+
+            template = tplEnv.get_template( "meta.jinja" )
+            meta = template.render(tplVars)
+
+            subject = metadata[2] + '¤' + metadata[1] + '¤' + kw[0] + '¤' + meta 
+
+            template = tplEnv.get_template( "body.jinja" )
+            body = template.render(tplVars)
+
+            sendpost(recipient, user, subject, body, metadata[3], server, password, debug)
+
+            cursor.execute("""
+            	UPDATE content SET posted = 1 WHERE id = ?
+            	""",(metadata[0],))
+
+         
+            conn.commit()
+            cursor.close()
+
+
+    except ValueError:
+        raise Exception( "Failed to send post")
+
+    except sqlite3.Error,e:
+        raise Exception( "Error in silk: %s" % e)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
